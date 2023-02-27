4baa837f094bfdd728277e444663d1ee18a2d1b3
Tazri <mdtazri@gmail.com>
2023-02-27 22:04:50 +0600

    I try to complete the one level string problem in beecrowd and I failed

--- problem_solving/string/beecrowd_level_1/1278.cs
+++ problem_solving/string/beecrowd_level_1/1278.cs
@@ -1,40 +1,84 @@
+/*
+Beecrowd dose not accept it
+*/
+
 using System;
 
 class JustifiedLeft{
-    static string readline(){
-        return Console.ReadLine();
-    }
-
-    static string getTrimLine(){
-        string line = readline();
-        string trimLine = "";
-        string words = "";
-        bool isStart = false;
-
-        for(int i = 0;i ){
-            
+    static void printSpace(int n,char space=' '){
+        for(int i = 0;i < n;i++){
+            Console.Write(space);
         }
     }
 
-    static void Main(){
-        string line = getTrimLine();
+    static string getLine(){
+        string line = Console.ReadLine();
+        string finalLine = "";
+        string word = "";
+        bool isWordStart = false;
 
-        Console.WriteLine($"Here is trimmed Line >{line}<");
+        for(int i = 0;i < line.Length;i++){
+            char c = line[i];
+            if(isWordStart){
+                if(c == ' '){
+                    isWordStart = false;
+
+                    if(finalLine == ""){
+                        finalLine = word;
+                    }else{
+                        finalLine += " " + word;
+                    }
+
+                    word = "";
+                }else{
+                    word += c;
+                }
+            }else{
+                if(c != ' '){
+                    isWordStart = true;
+                    word += c;
+                }
+            }
+        }
+
+        if(finalLine == ""){
+            finalLine = word;
+        }else{
+            if(word != ""){
+                finalLine += " " + word;
+            }
+        }
+
+        return finalLine;
+    }
+
+    static void Main(){
+        int totalLine = int.Parse(Console.ReadLine());
+
+        while(totalLine > 0){
+            string [] lineList = new string[totalLine];
+            
+            // get first one
+            lineList[0] = getLine();
+            int maxLen = lineList[0].Length;
+
+            for(int i = 1; i < totalLine;i++){
+                lineList[i] = getLine();
+
+                if(lineList[i].Length > maxLen){
+                    maxLen = lineList[i].Length;
+                }
+            }
+
+            foreach(string line in lineList){
+                int needSpace = maxLen - line.Length;
+
+                printSpace(needSpace);
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine();
+            totalLine = int.Parse(Console.ReadLine());
+        }
     }
 }
-
-/*
-     ROMEO      AND
-      JULIET WERE  
-        IN LOVE 
-
-3
-hello   afdaljd    afe      
-dajdfk  alfkje   afdlk   afe  afee 
-afjlaek      
-hello afdaljd afe
-dajdfk alfkje afdlk afe afee
-afjlaek
-
-
-*/
